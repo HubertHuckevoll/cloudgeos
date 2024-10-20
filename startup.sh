a0bb3675504b23c2e668d3ce6a977fb601ee3735
HubertHuckevoll <konstantin.meyer@gmail.com>
2024-10-20 15:40:35 +0200

    things work, but still not shutting down correctly

--- startup.sh
+++ startup.sh
@@ -8,17 +8,26 @@ export SDL_MOUSE_RELATIVE=0
 # Autostart configuration: Add the commands to run dosbox-staging
 mkdir -p /root/.config/lxsession/LXDE && echo "/root/pcgeos-basebox/binl64/basebox -conf /root/basebox.conf" > /root/.config/lxsession/LXDE/autostart
 
-# Launch xvfb
-/usr/bin/Xvfb :1 -screen 0 1024x768x16 +extension XTEST & \
+# Start Xvfb (Virtual framebuffer)
+/usr/bin/Xvfb :1 -screen 0 1024x768x16 +extension XTEST &
 
-# start LXDE, needed for mouse to work!
-startlxde & \
+# Start Polkit for authentication
+/usr/lib/policykit-1/polkitd --no-debug &
 
-# start X11VNC
-x11vnc -display :1 -rfbport 5901 -nopw -forever -noxrecord -noxfixes -grabptr -scale_cursor 1 & \
+# Start dbus (ensure dbus is running for lxsession)
+dbus-launch &
 
-# make sure display mouse is not accelerated (?)
-sleep 5 && xset -display :1 m 0 0 && \
+# Start LXDE session (including lxsession)
+/usr/bin/lxsession &
 
-# launch noVNC web frontend (client)
-/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 --web /opt/novnc
+# Start VNC server for remote access
+x11vnc -display :1 -rfbport 5901 -nopw -forever -noxrecord -noxfixes -grabptr -scale_cursor 1 &
+
+# Start noVNC for web-based access
+sleep 5 && /opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 --web /opt/novnc &
+
+# Start DOSBox-Staging
+#/root/pcgeos-basebox/binl64/basebox -conf /root/basebox.conf
+
+# Ensure the script waits for all background processes to finish
+wait
