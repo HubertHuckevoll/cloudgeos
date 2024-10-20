a7feb85aec40b90d165653e7e185062bf87d8af2
HubertHuckevoll <konstantin.meyer@gmail.com>
2024-10-20 16:10:14 +0200

    WIP polkit bug

--- startup.sh
+++ startup.sh
@@ -11,23 +11,23 @@ mkdir -p /root/.config/lxsession/LXDE && echo "/root/pcgeos-basebox/binl64/baseb
 # Start Xvfb (Virtual framebuffer)
 /usr/bin/Xvfb :1 -screen 0 1024x768x16 +extension XTEST &
 
+# Wait for Xvfb to initialize
+sleep 2
+
+# Start dbus (ensure dbus is running for lxsession)
+dbus-launch --exit-with-session &
+
 # Start Polkit for authentication
 /usr/lib/policykit-1/polkitd --no-debug &
 
-# Start dbus (ensure dbus is running for lxsession)
-dbus-launch &
-
 # Start LXDE session (including lxsession)
-/usr/bin/lxsession &
+lxsession &
 
 # Start VNC server for remote access
 x11vnc -display :1 -rfbport 5901 -nopw -forever -noxrecord -noxfixes -grabptr -scale_cursor 1 &
 
 # Start noVNC for web-based access
 sleep 5 && /opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 --web /opt/novnc &
-
-# Start DOSBox-Staging
-#/root/pcgeos-basebox/binl64/basebox -conf /root/basebox.conf
 
 # Ensure the script waits for all background processes to finish
 wait
